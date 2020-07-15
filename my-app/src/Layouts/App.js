c2fec6f86cb2de272f3f65050fdf6603fe2bc686
remi seris <remiseris@gmail.com>
2020-07-15 08:31:42 +0200

    clean repository

--- my-app/src/Layouts/App.js
+++ my-app/src/Layouts/App.js
@@ -1,30 +1,10 @@
-import React, { useState } from 'react';
-import List from '../components/List';
-import Header from '../components/Header';
-import ButtonConnection from '../components/Button'
-
-/* useState 
-Déclarer un variable détat
-Déclar un setter su cette variable
-Valeur de départ de cette variable
-
-*/
+import React from 'react';
 
 const App = () => {
-    const [connect, setConnection] = useState(false)
-
-    const toogleConnection = () => {
-        // On set la valeur de la variable à l'inverse de ça valeur actuelle
-        setConnection(!connect)
-        console.log(connect ? "Déconnection" : "Connection")
-    }
 
     return (
-        <div className={"layout"}>
-            <ButtonConnection connect={connect} toogleConnection={toogleConnection} />
-            <Header connect={connect} />
-
-            <List />
+        <div >
+            HELLO WORLD
         </div>)
 }
 
