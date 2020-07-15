0ae5c6c6a3c93523a6bc4e3bc46cededf0da4774
Sileina <49021819+Sileina@users.noreply.github.com>
2020-07-15 10:57:11 +0200

    Exo 1

--- my-app/src/Layouts/App.js
+++ my-app/src/Layouts/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Button from '../components/Button';
+import List from '../components/List';
+import Item from '../components/Item';
+
+
+const buttonTitleList = ['button 1', 'button 2', 'button 3', 'button 4']
 
 const App = () => {
+    const onButtonClick = (buttonName) => {
+        console.log('Click sur bouton ', buttonName);
+    }
+    
 
     return (
-        <div >
-            HELLO WORLD
-        </div>)
+        <div className='container'>
+            <Item></Item>
+
+        </div>
+        )
 }
 
 export default App;
